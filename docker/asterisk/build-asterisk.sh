f8880220908f62a59fe541adc8f99ea62ceef601
David M. Lee <dlee@respoke.io>
2015-09-16 11:41:36 -0500

    ubuntu-trusty normal build works

--- docker/asterisk/build-asterisk.sh
+++ docker/asterisk/build-asterisk.sh
@@ -15,7 +15,7 @@ set -ex
 mkdir -p /usr/src/asterisk
 cd /usr/src/asterisk
 
-curl -s http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-${ASTERISK_VERSION}.tar.gz |
+curl -sL http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-${ASTERISK_VERSION}.tar.gz |
     tar --strip-components 1 -xz
 
 ./configure --prefix=/usr/local
